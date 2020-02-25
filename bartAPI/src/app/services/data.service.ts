0a6f773a33a565bb5a87d7e94e6646bdf82f1ffc
Code <code@COCTL-41270.local>
2020-02-25 15:33:47 -0800

    app working again

--- bartAPI/src/app/services/data.service.ts
+++ bartAPI/src/app/services/data.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Stations } from '../interfaces/stations';
-import { Etds } from '../interfaces/etds';
 
 
 @Injectable({
